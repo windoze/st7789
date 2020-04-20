680455115a28295840303f5b06ad343f284f2fb8
Ales Katona <ales@katona.me>
2020-04-19 18:34:50 -0600

    formatting fix

--- src/batch.rs
+++ src/batch.rs
@@ -210,7 +210,10 @@ impl<P: Iterator<Item = Pixel<Rgb565>>> Iterator for RowIterator<P> {
                         continue;
                     }
                     //  If this pixel is adjacent to the previous pixel, add to the row.
-                    if x == self.x_right.wrapping_add(1) && y == self.y && self.colors.push(color).is_ok() {
+                    if x == self.x_right.wrapping_add(1)
+                        && y == self.y
+                        && self.colors.push(color).is_ok()
+                    {
                         // Don't add pixel if too many pixels in the row.
                         self.x_right = x;
                         continue;
